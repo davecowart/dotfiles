adc12214be6416591fbcb487d87a5ab8b76e950e
Dave Cowart <davecowart@gmail.com>
2017-02-09 16:52:00 -0600

    add macos defaults

---
+++ macos/set-defaults.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# increase bluetooth sound quality
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+# enable Tab in modal dialogs
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
+
+# Disable automatic emoji substitution (i.e. use plain text smileys)
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
+
+# Disable smart quotes as it’s annoying for messages that contain code
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
+
+

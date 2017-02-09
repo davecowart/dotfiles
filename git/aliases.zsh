294f49dde62f7dcf5e683ab9cc68436db2b03a11
Dave Cowart <davecowart@gmail.com>
2017-02-08 21:57:27 -0600

    fix git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,5 +1,5 @@
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
-alias gst=git status -sb
-alias gp=git pull --rebase
-alias gb=git branch
+alias gst='git status -sb'
+alias gp='git pull --rebase'
+alias gb='git branch'
 alias glog='git log --format="%Cgreen%h%Creset %C(cyan)%an%Creset - %s %Cgreen(%cr)%Creset" --gra    ph --date=relative'

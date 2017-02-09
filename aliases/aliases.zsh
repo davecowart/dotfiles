33bc2a9689662dc2f53942d3daecfcda3ccef5f0
Dave Cowart <davecowart@gmail.com>
2017-02-09 09:42:21 -0600

    add aliases and configs from work machine

---
+++ aliases/aliases.zsh
@@ -0,0 +1,3 @@
+alias server='python -m SimpleHTTPServer 3000 & open http://localhost:3000 && fg'
+alias speedtest='wget --report-speed=bits -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip'
+alias up=dot

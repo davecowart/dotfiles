c61ca518a09928f425cbf97029d029729f6a4c31
Dave Cowart <davecowart@gmail.com>
2017-02-09 10:52:16 -0600

    add bundle and jekyll aliases

--- ruby/ruby.zsh
+++ ruby/ruby.zsh
@@ -1,3 +1,7 @@
 export PORT=3000
 
 alias rake='noglob rake'
+alias be='bundle exec'
+alias bi='bundle install'
+alias bu='bundle update'
+alias jek='bundle exec jekyll serve & sleep 5 && open http://localhost:4000 && fg'

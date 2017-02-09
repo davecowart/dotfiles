a22b08c4d249401f39d557c1f570a013f5195938
Dave Cowart <davecowart@gmail.com>
2017-02-09 10:52:24 -0600

    add reload alias

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -1,2 +1,4 @@
 alias server='python -m SimpleHTTPServer 3000 & open http://localhost:3000 && fg'
 alias speedtest='wget --report-speed=bits -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip'
+alias reload='. ~/.zshrc'
+

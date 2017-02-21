af881c06a4efa23bea0096f84b410f4278ee78fc
Dave Cowart <davecowart@gmail.com>
2017-02-21 16:26:02 -0600

    add weather alias

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -1,4 +1,5 @@
 alias server='python -m SimpleHTTPServer 3000 & open http://localhost:3000 && fg'
 alias speedtest='wget --report-speed=bits -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip'
 alias reload='. ~/.zshrc'
+alias weather='curl -4 http://wttr.in/'
 

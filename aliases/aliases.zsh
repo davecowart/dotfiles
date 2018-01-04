18e486bf745475944ba23e2eb2c0c377bc36c5d6
Dave Cowart <davecowart@gmail.com>
2018-01-04 10:19:35 -0600

    fix language for moon alias

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -2,7 +2,7 @@ alias server='python -m SimpleHTTPServer 3000 & open http://localhost:3000 && fg
 alias speedtest='wget --report-speed=bits -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip'
 alias reload='. ~/.zshrc'
 alias weather='curl http://wttr.in/'
-alias moon='curl http://wttr.in/moon'
+alias moon='curl http://us.wttr.in/moon'
 alias flushdns='sudo killall -HUP mDNSResponder'
 alias whodat='whois -h whois.arin.net'
 
